a98ce0786ec62639ec42c10610aebead65eb7fdd
Vallary A. Obare <wanembavallary@gmail.com>
2025-07-14 12:32:13 +0300

    fix output text an push multi-language script

---
+++ 1-multi_languages.js
@@ -0,0 +1,9 @@
+//1-multi_languages.js
+const message1 = "C is fun";
+const message2 = "Python is cool";
+const message3 = "JavaScript is amazing";
+//1-multi_language.js
+console.log(message1);
+console.log(message2);
+console.log(message3);
+ 
